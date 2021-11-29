a34b8fbf30d9fea2ee72ae73210cffa140f2a16c
0ffs3c <asuryateja14@gmail.com>
2021-11-29 11:39:09 +0530

    Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, {useState} from 'react'
+import '../../node_modules/react-toastify/dist/ReactToastify.css'
+import { ToastContainer, toast } from 'react-toastify';
 import './Header.css'
 import ipfs from './ipfs.svg'
 import {Link} from 'react-router-dom'
+const Web3 = require('web3')
+
+
+
+
 
 function Header() {
+    const [text, setText] = useState('Connect')
+    const [connected, setConnected] = useState(false)
+    const ethEnabled = async () => {
+        if (typeof window.ethereum !== 'undefined') {
+            const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+            setText(accounts[0]);
+            setConnected(true)
+          }
+          else{
+            toast.dark('Please Install MetaMask', {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                });
+          }
+    }
+
     return (
         <>
             <nav class="navbar  navbar-custom">
@@ -15,11 +43,15 @@ function Header() {
                             className="filter-green"
                             loading="lazy"
                         /></Link>
-                        <div class='d-flex flex-row'>
-                        <Link style={{ textDecoration: 'none' , fontWeight: 'bold', color: 'white', paddingLeft: '15px'}} to='/upload'>Upload</Link>
-                        <Link style={{ textDecoration: 'none' , fontWeight: 'bold', color: 'white', paddingLeft: '15px', paddingRight: '15px'}} to='/stream'>Video</Link>
-                        <Link style={{ textDecoration: 'none' , fontWeight: 'bold', color: 'white', paddingRight: '15px'}} to='/photo'>Photo</Link>
-                        
+                        <div class='d-flex flex-row align-items-center'>
+                     {
+                         connected ?<div> <Link style={{ textDecoration: 'none' , fontWeight: 'bold', color: 'white', paddingLeft: '15px'}} to='/upload'>Upload</Link>
+                         <Link style={{ textDecoration: 'none' , fontWeight: 'bold', color: 'white', paddingLeft: '15px', paddingRight: '15px'}} to='/stream'>Video</Link>
+                         <Link style={{ textDecoration: 'none' , fontWeight: 'bold', color: 'white', paddingRight: '15px'}} to='/photo'>Photo</Link>
+                         <button type="button" id='loginButton' class='bts' onClick={ethEnabled} style={{borderRadius: '10px', padding: "5px", fontWeight: 'bold', marginLeft: '10px', marginRight: '10px'}}><span class="blink_me"></span>{ ' '+ text}</button>
+                         </div> : <button type="button" id='loginButton' onClick={ethEnabled} style={{borderRadius: '10px', padding: "5px", fontWeight: 'bold', marginLeft: '10px', marginRight: '10px'}}>{text}</button>
+                     }
+                                               
                         </div>
                 </div>
             </nav>
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import './Main.css'
 import Lottie from "react-lottie";
+import { ToastContainer, toast } from 'react-toastify';
+import '../../node_modules/react-toastify/dist/ReactToastify.css'
 import animationData from './blockchain.json'
 import Typicaal from './Typicaal';
 import About from './About';
 
 function Main() {
+
     const defaultOptions = {
         loop: true,
         autoplay: true,
@@ -19,6 +22,17 @@ function Main() {
         <>
         <Typicaal />
         <div class='container pt-5'>
+        <ToastContainer style = {{marginTop: "5%"}}
+position="top-right"
+autoClose={5000}
+hideProgressBar={false}
+newestOnTop={false}
+closeOnClick
+rtl={false}
+pauseOnFocusLoss
+draggable
+pauseOnHover
+/>
         <Lottie options={defaultOptions} height={400} width={400} />
         <About />
         </div>
